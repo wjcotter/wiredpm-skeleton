185ad12757e3764cc29e1ce21ceda934e5a28b78
Bill Cotter <wjcotter@rocketmail.com>
2012-11-20 12:30:02 -0500

    initial commit

---
+++ local-config-sample.php
@@ -0,0 +1,14 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'wiredpm-skeleton' );
+define( 'DB_USER', 'bill' );
+define( 'DB_PASSWORD', 'Irish510' );
+define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+//define( 'RELOCATE', true);
